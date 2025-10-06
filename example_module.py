1c82c6450b0d5b447ccfcc9ac912952d2d640eec
Martin Lorentzon <106841201+martin-lorentzon@users.noreply.github.com>
2025-10-06 22:48:01 +0200

    LoadSomeDataOP

--- example_module.py
+++ example_module.py
@@ -1,4 +1,6 @@
 import bpy
+import addon_utils
+from pathlib import Path
 
 
 # ——————————————————————————————————————————————————————————————————————
@@ -24,6 +26,7 @@ class ExamplePanel(bpy.types.Panel):
         layout = self.layout
         
         layout.label(text="Example Text")
+        layout.operator("wm.load_data")
 
 
 # ——————————————————————————————————————————————————————————————————————
@@ -31,3 +34,25 @@ class ExamplePanel(bpy.types.Panel):
 # ——————————————————————————————————————————————————————————————————————
 
 
+class LoadSomeDataOP(bpy.types.Operator):
+    bl_idname = "wm.load_data"
+    bl_label  = "Load Data"
+    
+    def execute(self, context):
+        filepath = None
+        
+        for mod in addon_utils.modules():
+            if mod.bl_info["name"] == "Example Add-on":
+                filepath = Path(mod.__file__).parent / ("Stylized_Fire.blend")
+        
+        with bpy.data.libraries.load(str(filepath)) as (data_from, data_to):
+            data_to.objects = data_from.objects
+            # Eller med mer kontroll
+            # data_to.objects = [o for o in data_from.objects if o.name == "Stylized Fire"]
+            
+        for obj in data_to.objects:
+            context.scene.collection.objects.link(obj)
+            
+            obj.modifiers["GeometryNodes"]["Socket_2"] = 6
+        
+        return {"FINISHED"}
