48da05b6b5bd63fe97e16efb8b617b9ba9da8296
Martin Lorentzon <106841201+martin-lorentzon@users.noreply.github.com>
2025-10-08 09:41:46 +0200

    Update example_module.py

--- example_module.py
+++ example_module.py
@@ -41,18 +41,22 @@ class LoadSomeDataOP(bpy.types.Operator):
     def execute(self, context):
         filepath = None
         
+        # Hittar filvägen till verktygets add-on-mapp (och blend-filen)
         for mod in addon_utils.modules():
             if mod.bl_info["name"] == "Example Add-on":
                 filepath = Path(mod.__file__).parent / ("Stylized_Fire.blend")
         
+        # Laddar blend-filen som ett blend-bibliotek och dess innehåll som vi specifierar
         with bpy.data.libraries.load(str(filepath)) as (data_from, data_to):
             data_to.objects = data_from.objects
-            # Eller med mer kontroll
-            # data_to.objects = [o for o in data_from.objects if o.name == "Stylized Fire"]
+            # Eller mer selektivt:
+            # data_to.objects = ["Stylized Fire"]
             
+        # Länkar in objekten i den aktiva scenen
         for obj in data_to.objects:
             context.scene.collection.objects.link(obj)
             
+            # Ändrar en parameter i Geometry Nodes-modifieraren (exempel om sådan finns)
             obj.modifiers["GeometryNodes"]["Socket_2"] = 6
         
         return {"FINISHED"}
